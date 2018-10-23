96bef9e7a98abf4042c18f2c060d3b079b8a64f1
HaoWu-NEU <wu.hao10@husky.neu.edu>
2018-10-22 21:13:58 -0400

    upload

--- Toy/util_hmm_variational_gibbs.py
+++ Toy/util_hmm_variational_gibbs.py
@@ -185,39 +185,38 @@ def log_joint(alpha_init_0, alpha_trans_0, nu_0, W_0, m_0, beta_0, Zs, Pi, A, mu
     return log_joint_prob
 
 
-def inclusive_kl(A_samples, latents_dirs, alpha_init_0, alpha_trans_0, nu_0, W_0, m_0, beta_0, Zs, Pi, mu_ks, cov_ks, Y, T, D, K, num_particles):
-    log_p_joint = torch.zeros(num_particles)
-    for n in range(num_particles):
+def inclusive_kl(A_samples, latents_dirs, alpha_init_0, alpha_trans_0, nu_0, W_0, m_0, beta_0, Zs, Pi, mu_ks, cov_ks, Y, T, D, K, num_particles_encoder):
+    log_p_joint = torch.zeros(num_particles_encoder)
+    for n in range(num_particles_encoder):
         A = A_samples[:, n, :]
         log_p_joint[n] = log_joint(alpha_init_0, alpha_trans_0, nu_0, W_0, m_0, beta_0, Zs, Pi, A, mu_ks, cov_ks, Y, T, D, K).item()
 
-    log_p_cond = torch.zeros((K, num_particles))
+    log_p_cond = torch.zeros((K, num_particles_encoder))
     alpha_trans_hat = alpha_trans_0 + pairwise(Zs, T).sum(0)
-    kl = 0.0
+    kl = kl_dirichlets(alpha_trans_0, latents_dirs, Zs, T, K)
     for k in range(K):
         log_p_cond[k] = Dirichlet(alpha_trans_hat[k]).log_prob(A_samples[k])
-        kl += kl_dirichlet(alpha_trans_hat[k], latents_dirs[k])
     #
     log_p_cond = log_p_cond.sum(0)
-    log_q = torch.zeros((K, num_particles))
-    for k in range(K):
-        log_q[k] = Dirichlet(latents_dirs[k]).log_prob(A_samples[k])
-    log_q = log_q.sum(0)
+    log_q = log_q_hmm(latents_dirs, A_samples, K, num_particles_encoder)
     log_weights = log_p_joint - log_q - log_sum_exp(log_p_joint - log_q)
     log_weights = log_weights.detach()
     weights = torch.exp(log_weights)
     loss_inference = - torch.mul(weights, log_q).sum()
     ess = (1. / (weights ** 2 ).sum()).item()
     kl_est = torch.mul(weights, log_p_cond - log_q).sum().detach().item()
-    return loss_inference, kl, kl_est, ess, weights
+    return loss_inference, kl, kl_est, ess
 
-def log_q_hmm(latents_dirs, A_samples, K):
-    log_q = torch.zeros(K)
+def log_q_hmm(latents_dirs, A_samples, K, num_particles):
+    log_q = torch.zeros((K, num_particles))
     for k in range(K):
         log_q[k] = Dirichlet(latents_dirs[k]).log_prob(A_samples[k])
-    return log_q.sum()
+    if num_particles == 1:
+        return log_q.sum()
+    else:
+        return log_q.sum(0)
 
-def kl_dirichlets(alpha_trans_0, Zs, T, K):
+def kl_dirichlets(alpha_trans_0, latents_dirs, Zs, T, K):
     alpha_trans_hat = alpha_trans_0 + pairwise(Zs, T).sum(0)
     kl = 0.0
     for k in range(K):
@@ -230,3 +229,60 @@ def kl_dirichlet(alpha1, alpha2):
     C = (torch.mul(alpha1 - alpha2, torch.digamma(alpha1) - torch.digamma(alpha1.sum()))).sum()
     kl = A - B + C
     return kl
+
+# def inclusive_kl2(enc, A_samples, latents_dirs, alpha_init_0, alpha_trans_0, nu_0, W_0, m_0, beta_0, Zs, Pi, mu_ks, cov_ks, Y, T, D, K, num_particles_rws, num_particles_encoder, num_particles_smc):
+#     log_weights_rws = torch.zeros(num_particles_rws)
+#     log_qs = torch.zeros(num_particles_rws)
+#     log_p_conds = torch.zeros(num_particles_rws)
+#     kl = 0.0
+#     for l in range(num_particles_rws):
+#         # sample a obs sequence
+# #         Y, mu_true, cov_true, Zs_true, Pi_true, A_true = sampling_hmm(T, K, D)
+#         # initialize A from prior
+# #         A_samples = initial_trans(alpha_trans_0, K)
+#         # SMC to generate a weighted sample set for local states
+#         Zs, log_weights, log_normalizer = smc_hmm(Pi, A_samples, mu_ks, cov_ks, Y, T, D, K, num_particles_smc)
+#         # draw a sample from the sample set
+#         Z_ret = resampling_smc(Zs, log_weights)
+#         latents_dirs, A_samples_new = enc(Z_ret.contiguous().view(-1, T*K), num_particles_encoder)
+#         loss_inference, kl, kl_est, ess, weights = inclusive_kl(A_samples_new, latents_dirs, alpha_init_0, alpha_trans_0, nu_0, W_0, m_0, beta_0, Zs, Pi, mu_ks, cov_ks, Y, T, D, K, num_particles_encoder)
+#         log_p_joint = torch.zeros(num_particles_encoder)
+#         for n in range(num_particles_encoder):
+#             A = A_samples[:, n, :]
+#             log_p_joint[n] = log_joint(alpha_init_0, alpha_trans_0, nu_0, W_0, m_0, beta_0, Zs, Pi, A, mu_ks, cov_ks, Y, T, D, K).item()
+#
+#         log_p_cond = torch.zeros((K, num_particles_rws))
+#         alpha_trans_hat = alpha_trans_0 + pairwise(Z_ret, T).sum(0)
+#     kl = kl_dirichlets(alpha_trans_0, latents_dirs, Zs, T, K)
+#     for k in range(K):
+#         log_p_cond[k] = Dirichlet(alpha_trans_hat[k]).log_prob(A_samples[k])
+#     #
+#     log_p_cond = log_p_cond.sum(0)
+#     log_q = log_q_hmm(latents_dirs, A_samples, K, num_particles_rws)
+#     log_weights = log_p_joint - log_q - log_sum_exp(log_p_joint - log_q)
+#     log_weights = log_weights.detach()
+#     weights = torch.exp(log_weights)
+#     loss_inference = - torch.mul(weights, log_q).sum()
+#     ess = (1. / (weights ** 2 ).sum()).item()
+#     kl_est = torch.mul(weights, log_p_cond - log_q).sum().detach().item()
+#
+#
+#         log_p_joint_curr = log_joint(alpha_init_0, alpha_trans_0, nu_0, W_0, m_0, beta_0, Z_ret, Pi, A_samples_new, mu_ks, cov_ks, Y, T, D, K).detach().item()
+#
+#
+#         log_p_cond = 0.0
+#         alpha_trans_hat = alpha_trans_0 + pairwise(Z_ret, T).sum(0)
+#         for k in range(K):
+#             log_p_cond += Dirichlet(alpha_trans_hat[k]).log_prob(A_samples_new[k])
+#         kl += kl_dirichlets(alpha_trans_0, latents_dirs, Z_ret, T, K)
+#         log_p_conds[l] = log_p_cond
+#
+#         log_qs[l] = log_q_hmm(latents_dirs, A_samples_new, K)
+#         log_weights_rws[l] = log_p_joint_curr - log_q_hmm(latents_dirs, A_samples_new, K) + log_normalizer
+#     kl /= num_particles_rws
+#
+#     log_weights_rws = (log_weights_rws - log_sum_exp(log_weights_rws)).detach()
+#     weights_rws = torch.exp(log_weights_rws)
+#     ess = (1. / (weights_rws ** 2 ).sum()).item()
+#     loss_infer = - torch.mul(weights_rws, log_qs).sum()
+#     kl_est = torch.mul(weights_rws, log_p_conds - log_qs).sum().detach().item()
