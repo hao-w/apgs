983531de84de46c5a5936e560fd0eb4cb806b838
hao-w <wuhaomxhy@gmail.com>
2018-11-09 12:58:41 -0500

    change twoshots encoder

--- AmortizedGibbs/disps/baseline_smc.py
+++ AmortizedGibbs/disps/baseline_smc.py
@@ -56,7 +56,7 @@ def kl_dirichlet(alpha1, alpha2):
     kl = A - B + C
     return kl
 
-def onesmc_sampling(enc, alpha_trans_0, Pi, mu_ks, cov_ks, Z_true, Y, T, D, K, num_particles, num_particles_smc):
+def oneshot_sampling(enc, alpha_trans_0, Pi, mu_ks, cov_ks, Z_true, Y, T, D, K, num_particles, num_particles_smc):
     log_weights_is = torch.zeros(num_particles)
     log_p_joints = torch.zeros(num_particles)
     kls = torch.zeros(num_particles)
@@ -79,32 +79,35 @@ def onesmc_sampling(enc, alpha_trans_0, Pi, mu_ks, cov_ks, Z_true, Y, T, D, K, n
     elbo = log_weights_is.mean()
     return enc, eubo, kl, ess, latents_dirs, elbo
 
-def twoshots_sampling(A_true, enc, alpha_trans_0, Pi, mu_ks, cov_ks, Z_true, Y, T, D, K, num_particles, num_particles_smc):
-    log_weights_is = torch.zeros(num_particles)
+def twoshots_sampling(enc_trans, enc_disp, alpha_trans_0, Pi, mu_ks, cov_ks, Z_true, Y, T, D, K, num_particles, num_particles_smc):
+    log_weights_rws = torch.zeros(num_particles)
     log_p_joints = torch.zeros(num_particles)
     kls = torch.zeros(num_particles)
     for l in range(num_particles):
-        # Y_pairwise = torch.cat((Y[:-1].unsqueeze(0), Y[1:].unsqueeze(0)), 0).transpose(0, 1).contiguous().view(T-1, 2*D)
-        # latents_dirs, A_sample = enc(Y_pairwise)
-        Zs, log_weights, log_normalizer = smc_hmm(Pi, A_true, mu_ks, cov_ks, Y, T, D, K, num_particles_smc)
+        Y_pairwise = torch.cat((Y[:-1].unsqueeze(0), Y[1:].unsqueeze(0)), 0).transpose(0, 1).contiguous().view(T-1, 2*D)
+        latents_dirs_disp, A_sample_disp = enc_disp(Y_pairwise)
+        Zs, log_weights, log_normalizer = smc_hmm(Pi, A_sample_disp, mu_ks, cov_ks, Y, T, D, K, num_particles_smc)
         Z_ret = resampling_smc(Zs, log_weights)
-        log_q_smc = log_joint_smc(Z_ret, Pi, A_true, mu_ks, cov_ks, Y, T, D, K)
+        log_p_joints = log_joint(alpha_trans_0, Z_ret, Pi, A_sample_disp, mu_ks, cov_ks, Y, T, D, K).detach()
+        log_q_smc = log_joint_smc(Z_ret, Pi, A_sample_disp, mu_ks, cov_ks, Y, T, D, K)
+        log_q_enc_disp = log_q_hmm(latents_dirs_disp, A_sample_disp)
+        log_weights_rws[l] = log_p_joints - log_q_smc - log_q_enc_disp + log_normalizer
+
         Z_pairwise = torch.cat((Z_ret[:-1].unsqueeze(0), Z_ret[1:].unsqueeze(0)), 0).transpose(0, 1).contiguous().view(T-1, 2*K)
-        latents_dirs, A_sample = enc(Z_pairwise)
-        log_q_mlp = log_q_hmm(latents_dirs, A_sample)
+        latents_dirs_trans, A_sample_trans = enc_trans(Z_pairwise)
+        log_p_joints_target = log_joint(alpha_trans_0, Z_ret, Pi, A_sample_trans, mu_ks, cov_ks, Y, T, D, K).detach()
+        log_q_forward = log_q_hmm(latents_dirs_trans, A_sample_trans)
+        log_q_reverse = log_q_hmm(latents_dirs_trans, A_sample_disp)
+        log_weights_rws[l] = log_weights_rws[l] + log_p_joints_target - log_p_joints + log_q_reverse - log_q_forward
 
-        log_p_joints = log_joint(alpha_trans_0, Z_ret, Pi, A_sample, mu_ks, cov_ks, Y, T, D, K).detach()
-        log_weights_is[l] = log_p_joints - log_q_smc - log_q_mlp + log_normalizer
-        kls[l] = kl_dirichlets(alpha_trans_0, latents_dirs, Z_ret, T, K)
+        Zs, log_weights, log_normalizer = csmc_hmm(Z_ret, Pi, A_sample_trans, mu_ks, cov_ks, Y, T, D, K, num_particles_smc)
+        Z_ret = resampling_smc(Zs, log_weights)
 
-    log_weights_is_norm = log_weights_is - logsumexp(log_weights_is, dim=0)
-    weights_is = torch.exp(log_weights_is_norm).detach()
-    kl =  torch.mul(weights_is, kls).sum()
-    ess = (1. / (weights_is ** 2 ).sum()).item()
-    eubo =  torch.mul(weights_is, log_weights_is).sum()
-<<<<<<< HEAD:AmortizedGibbs/disps/baseline_smc.py
-    return enc, eubo, kl, ess, latents_dirs
-=======
-    elbo = log_weights_is.sum()
-    return enc, eubo, kl, ess, latents_dirs, elbo
->>>>>>> 9fa5e5028ad3ebbea666020006d2621e092d74a7:AmortizedGibbs/BNP/baseline_smc.py
+        kls[l] = kl_dirichlets(alpha_trans_0, latents_dirs_trans, Z_ret, T, K)
+
+    weights_rws = torch.exp(log_weights_rws - logsumexp(log_weights_rws, dim=0)).detach()
+    kl =  kls.mean()
+    ess = (1. / (weights_rws ** 2 ).sum()).item()
+    eubo =  torch.mul(weights_rws, log_weights_rws).sum()
+    elbo = log_weights_rws.mean()
+    return eubo, kl, ess, latents_dirs_trans, elbo
--- AmortizedGibbs/pixels/.ipynb_checkpoints/util_plots-checkpoint.py
+++ AmortizedGibbs/pixels/.ipynb_checkpoints/util_plots-checkpoint.py
@@ -7,16 +7,19 @@ import matplotlib.gridspec as gridspec
 def pairwise(Zs, T):
     return torch.bmm(Zs[:T-1].unsqueeze(-1), Zs[1:].unsqueeze(1))
 
-def plot_dirs(latents_dirs, alpha_trans_0, Zs_true, T, K, vmax):
-    conjugate_post = alpha_trans_0 + pairwise(Zs_true, T).sum(0)
+def plot_dirs(enc, alpha_trans_0, Z_ret, Zs_true, T, K, num_particles_rws, vmax):
+    Z_ret_pair = torch.cat((Z_ret[:, :T-1, :].unsqueeze(1), Z_ret[:, 1:, :].unsqueeze(1)), 1)
+    ind = np.random.randint(num_particles_rws)
+    latents_dir, A_sample = enc(Z_ret_pair[ind].transpose(0,1).contiguous().view(T-1, 2*K))
+    conjugate_post = alpha_trans_0 + pairwise(torch.from_numpy(Zs_true).float(), T).sum(0)
     print('variational : ')
-    print(latents_dirs)
+    print(latents_dir)
     print('conjugate posterior :')
     print(conjugate_post)
 
     fig3 = plt.figure(figsize=(12,6))
     ax1 = fig3.add_subplot(1, 2, 1)
-    infer_plot = ax1.imshow(latents_dirs.data.numpy(), cmap='viridis', vmin=0, vmax=vmax)
+    infer_plot = ax1.imshow(latents_dir.data.numpy(), cmap='viridis', vmin=0, vmax=vmax)
     ax1.set_xticks([])
     ax1.set_yticks([])
     ax1.set_title('variational')
@@ -26,22 +29,23 @@ def plot_dirs(latents_dirs, alpha_trans_0, Zs_true, T, K, vmax):
     ax2.set_yticks([])
     ax2.set_title('conjugate posterior')
     cax = fig3.add_axes([1.0, 0.15, 0.03, 0.7])
-    #fig3.colorbar(true_plot, cax=cax, orientation='vertical')
+    fig3.colorbar(true_plot, cax=cax, orientation='vertical')
     # cbaxes = fig3.add_axes([0.95, 0.32, 0.02, 0.36])
     # cb = plt.colorbar(true_plot, cax = cbaxes)
     # fig3.savefig('transition_plot T=%d_series=%d_boundary=%d_ratio=%f.png' % (T, num_series, Boundary, signal_noise_ratio))
 
-def plot_results(EUBOs, ELBOs, log_qs, KLs, ESSs):
+def plot_results(EUBOs, log_qs, KLs, ESSs, num_particles_rws):
     fig, ax = plt.subplots(figsize=(16, 8))
     ax1 = fig.add_subplot(1,2,1)
     x = np.arange(len(EUBOs))
     ax1.plot(EUBOs, 'r-', label='eubo')
-    ax1.plot(ELBOs, 'b-', label='- log_q')
+    ax1.plot(log_qs, 'b-', label='- log_q')
     ax1.plot(KLs, 'g-', label='KL')
     ax1.legend()
     ax1.set_xlabel('epochs')
     ax2 = fig.add_subplot(1,2,2)
-    ax2.plot(np.array(ESSs))
+    ax2.plot(np.array(ESSs) / num_particles_rws)
+    plt.show()
 
 
 def plot_smc_sample(Zs_true, Zs_ret):
