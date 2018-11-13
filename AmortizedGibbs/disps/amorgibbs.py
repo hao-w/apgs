2fed70bbe6f561a47aaf00d61d9849c9263875d6
hao-w <wuhaomxhy@gmail.com>
2018-11-13 12:07:57 -0500

    upload other sampling method

--- AmortizedGibbs/disps/amorgibbs.py
+++ AmortizedGibbs/disps/amorgibbs.py
@@ -38,39 +38,6 @@ def rws_nested(enc, alpha_trans_0, Pi, mu_ks, cov_ks, Y, T, D, K, num_particles_
     elbo = log_weights_rws.mean()
     return eubo, kl, ess, variational, elbo
 
-def rws_decompose2(enc, alpha_trans_0, Pi, mu_ks, cov_ks, Y, T, D, K, num_particles_rws, num_particles_smc, mcmc_steps):
-    log_weights_rws = torch.zeros(num_particles_rws)
-    log_p_joints = torch.zeros(num_particles_rws)
-    log_p_smcs = torch.zeros(num_particles_rws)
-    log_qs = torch.zeros(num_particles_rws)
-    kls = torch.zeros(num_particles_rws)
-    for l in range(num_particles_rws):
-        A_samples = initial_trans(alpha_trans_0, K)
-        log_weights_rws[l] = log_weights_rws[l] - log_q_hmm(alpha_trans_0, A_samples)
-        Zs, log_weights, log_normalizer = smc_hmm(Pi, A_samples, mu_ks, cov_ks, Y, T, D, K, num_particles_smc)
-        Z_ret = resampling_smc(Zs, log_weights)
-        Z_ret_pairwise = torch.cat((Z_ret[:T-1].unsqueeze(0), Z_ret[1:].unsqueeze(0)), 0).transpose(0, 1).contiguous().view(T-1, 2*K)
-        for m in range(mcmc_steps):
-            A_prev = A_samples.clone()
-            variational_curr, A_samples = enc(Z_ret_pairwise, alpha_trans_0)
-            log_weights_rws[l] = log_weights_rws[l] - log_q_hmm(variational_curr, A_samples)
-            Zs, log_weights, log_normalizer = smc_hmm(Pi, A_samples, mu_ks, cov_ks, Y, T, D, K, num_particles_smc)
-            Z_ret = resampling_smc(Zs, log_weights)
-            Z_ret_pairwise = torch.cat((Z_ret[:T-1].unsqueeze(0), Z_ret[1:].unsqueeze(0)), 0).transpose(0, 1).contiguous().view(T-1, 2*K)
-            variational_curr, A_samples = enc(Z_ret_pairwise, alpha_trans_0)
-            log_q_prev = log_q_hmm(variational_curr, A_prev)
-            log_weights_rws[l] = log_weights_rws[l] + log_q_prev
-        log_weights_rws[l] = log_weights_rws[l] + log_normalizer + log_q_hmm(alpha_trans_0, A_samples)
-        posterior = alpha_trans_0 + pairwise(Z_ret, T).sum(0)
-        kls[l] = kl_dirichlets(posterior, variational_curr, K)
-
-    weights_rws = torch.exp(log_weights_rws - logsumexp(log_weights_rws, dim=0)).detach()
-    kl =  kls.mean()
-    ess = (1. / (weights_rws ** 2 ).sum()).item()
-    eubo =  torch.mul(weights_rws, log_weights_rws).sum()
-    elbo = log_weights_rws.mean()
-    return eubo, kl, ess, variational_curr, elbo
-
 def rws_sis(enc, alpha_trans_0, Pi, mu_ks, cov_ks, Y, T, D, K, num_particles_rws, num_particles_smc, mcmc_steps):
     log_weights_sis = torch.zeros(num_particles_rws)
     kls = torch.zeros(num_particles_rws)
@@ -139,8 +106,8 @@ def rws_decompose1(enc, alpha_trans_0, Pi, mu_ks, cov_ks, Y, T, D, K, num_partic
     elbo = log_weights_rws.mean()
     return eubo, kl, ess, variational_curr, elbo
 
-def rws_decompose1_rao(enc, alpha_trans_0, Pi, mu_ks, cov_ks, Y, T, D, K, num_particles_rws, num_particles_smc, mcmc_steps):
-    log_weights_rws = torch.zeros((num_particles_rws, mcmc_steps+1))
+def rws_decompose2(enc, alpha_trans_0, Pi, mu_ks, cov_ks, Y, T, D, K, num_particles_rws, num_particles_smc, mcmc_steps):
+    log_weights_rws = torch.zeros(num_particles_rws)
     log_p_joints = torch.zeros(num_particles_rws)
     log_p_smcs = torch.zeros(num_particles_rws)
     log_qs = torch.zeros(num_particles_rws)
@@ -151,18 +118,17 @@ def rws_decompose1_rao(enc, alpha_trans_0, Pi, mu_ks, cov_ks, Y, T, D, K, num_pa
         Zs, log_weights, log_normalizer = smc_hmm(Pi, A_samples, mu_ks, cov_ks, Y, T, D, K, num_particles_smc)
         Z_ret = resampling_smc(Zs, log_weights)
         Z_ret_pairwise = torch.cat((Z_ret[:T-1].unsqueeze(0), Z_ret[1:].unsqueeze(0)), 0).transpose(0, 1).contiguous().view(T-1, 2*K)
-        log_weights_rws[l] = log_weights_rws[l] + log_normalizer.clone()
-        log_p_curr = log_joint(alpha_trans_0, Z_ret, Pi, A_samples, mu_ks, cov_ks, Y, T, D, K).detach()
-
         for m in range(mcmc_steps):
             A_prev = A_samples.clone()
-            log_p_prev = log_p_curr.clone()
             variational_curr, A_samples = enc(Z_ret_pairwise, alpha_trans_0)
-            Zs, log_weights, log_normalizer = csmc_hmm(Z_ret, Pi, A_samples, mu_ks, cov_ks, Y, T, D, K, num_particles_smc)
+            log_weights_rws[l] = log_weights_rws[l] - log_q_hmm(variational_curr, A_samples)
+            Zs, log_weights, log_normalizer = smc_hmm(Pi, A_samples, mu_ks, cov_ks, Y, T, D, K, num_particles_smc)
             Z_ret = resampling_smc(Zs, log_weights)
             Z_ret_pairwise = torch.cat((Z_ret[:T-1].unsqueeze(0), Z_ret[1:].unsqueeze(0)), 0).transpose(0, 1).contiguous().view(T-1, 2*K)
-            log_p_curr = log_joint(alpha_trans_0, Z_ret, Pi, A_samples, mu_ks, cov_ks, Y, T, D, K).detach()
-            log_weights_rws[l] = log_weights_rws[l] - log_q_hmm(variational_curr, A_samples) + log_q_hmm(variational_curr, A_prev) + log_p_curr.clone() - log_p_prev.clone()
+            variational_curr, A_samples = enc(Z_ret_pairwise, alpha_trans_0)
+            log_q_prev = log_q_hmm(variational_curr, A_prev)
+            log_weights_rws[l] = log_weights_rws[l] + log_q_prev
+        log_weights_rws[l] = log_weights_rws[l] + log_normalizer + log_q_hmm(alpha_trans_0, A_samples)
         posterior = alpha_trans_0 + pairwise(Z_ret, T).sum(0)
         kls[l] = kl_dirichlets(posterior, variational_curr, K)
 
@@ -171,4 +137,34 @@ def rws_decompose1_rao(enc, alpha_trans_0, Pi, mu_ks, cov_ks, Y, T, D, K, num_pa
     ess = (1. / (weights_rws ** 2 ).sum()).item()
     eubo =  torch.mul(weights_rws, log_weights_rws).sum()
     elbo = log_weights_rws.mean()
+    return eubo, kl, ess, variational_curr, elbo
+
+def rws_decompose1_rao(enc, alpha_trans_0, Pi, mu_ks, cov_ks, Y, T, D, K, num_particles_rws, num_particles_smc, mcmc_steps):
+    log_weights_rws = torch.zeros((num_particles_rws, mcmc_steps+1))
+    kls = torch.zeros(num_particles_rws)
+
+    for l in range(num_particles_rws):
+        A_samples = initial_trans(alpha_trans_0, K)
+        Zs, log_weights, log_normalizer = smc_hmm(Pi, A_samples, mu_ks, cov_ks, Y, T, D, K, num_particles_smc)
+        Z_ret = resampling_smc(Zs, log_weights)
+        Z_ret_pairwise = torch.cat((Z_ret[:T-1].unsqueeze(0), Z_ret[1:].unsqueeze(0)), 0).transpose(0, 1).contiguous().view(T-1, 2*K)
+        log_weights_rws[l, 0] = log_normalizer.clone()
+        log_p_curr = log_joint(alpha_trans_0, Z_ret, Pi, A_samples, mu_ks, cov_ks, Y, T, D, K).detach()
+        for m in range(mcmc_steps):
+            A_prev = A_samples.clone()
+            log_p_prev = log_p_curr.clone()
+            variational_curr, A_samples = enc(Z_ret_pairwise, alpha_trans_0)
+            Zs, log_weights, log_normalizer = csmc_hmm(Z_ret, Pi, A_samples, mu_ks, cov_ks, Y, T, D, K, num_particles_smc)
+            Z_ret = resampling_smc(Zs, log_weights)
+            Z_ret_pairwise = torch.cat((Z_ret[:T-1].unsqueeze(0), Z_ret[1:].unsqueeze(0)), 0).transpose(0, 1).contiguous().view(T-1, 2*K)
+            log_p_curr = log_joint(alpha_trans_0, Z_ret, Pi, A_samples, mu_ks, cov_ks, Y, T, D, K).detach()
+            log_weights_rws[l, m+1] = log_p_curr - log_p_prev - log_q_hmm(variational_curr, A_samples) + log_q_hmm(variational_curr, A_prev)
+        posterior = alpha_trans_0 + pairwise(Z_ret, T).sum(0)
+        kls[l] = kl_dirichlets(posterior, variational_curr, K)
+
+    weights_rws = torch.exp(log_weights_rws - logsumexp(log_weights_rws, dim=0)).detach()
+    kl =  kls.mean()
+    ess = (1. / (weights_rws ** 2 ).sum(0))
+    eubo =  torch.mul(weights_rws, log_weights_rws).sum()
+    elbo = log_weights_rws.mean(0).sum()
     return eubo, kl, ess, variational_curr, elbo
