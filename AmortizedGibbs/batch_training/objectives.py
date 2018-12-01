2cb469e29cd3789fc890242d4aa331a33f99a5b8
hao-w <wuhaomxhy@gmail.com>
2018-12-01 17:52:23 -0500

    adaptive resampling, and stepwise weight sis

--- AmortizedGibbs/batch_training/objectives.py
+++ AmortizedGibbs/batch_training/objectives.py
@@ -54,40 +54,143 @@ def ag_decompose2(enc, prior_true, As_true, Zs_true, Pi, mu_ks, cov_ks, Ys, T, D
     loss = torch.mul(torch.exp(log_overall_weights).detach(), log_overall_weights).sum(1)
     return  loss.mean(), eubos.mean(), elbos.mean(), ess, kls.mean()
 
-def ag_adapt(enc, prior_true, As_true, Zs_true, Pi, mu_ks, cov_ks, Ys, T, D, K, num_particles_rws, num_particles_smc, mcmc_steps, batch_size):
-    log_overall_weights = torch.zeros((batch_size, num_particles_rws))
+def ag_sis_stepwise_adapt(enc, prior_true, As_true, Zs_true, Pi, mu_ks, cov_ks, Ys, T, D, K, num_particles_rws, num_particles_smc, mcmc_steps, batch_size):
+    log_uptonow_weights = torch.zeros((batch_size, mcmc_steps, num_particles_rws))
+    log_increment_weights = torch.zeros((batch_size, mcmc_steps, num_particles_rws))
     Zs_candidates = torch.zeros((num_particles_rws, batch_size, T, K))
-    As_candidates = torch.zeros((num_particles_rws, batch_size, K, K))
+    log_normalizers_candidates = torch.zeros((num_particles_rws, batch_size))
+
     for m in range(mcmc_steps):
         if m == 0:
             for l in range(num_particles_rws):
                 ## As B * K * K
                 As = initial_trans(prior_true, K, batch_size)
-                As_candidates[l] = As
+                Zs, log_weights, log_normalizers = smc_hmm_batch(Pi, As, mu_ks, cov_ks, Ys, T, D, K, num_particles_smc, batch_size)
+                log_normalizers_candidates[l] = log_normalizers
+                ## Z B * T * K
+                Z = smc_resamplings(Zs, log_weights, batch_size)
+                Zs_candidates[l] = Z
+                ## the first incremental weight is just log normalizer since As is sampled from prior
+                log_increment_weights[:, m, l] = log_normalizers
+                log_uptonow_weights[:, m, l] = log_normalizers
+        else:
+            Zs_prevs = Zs_candidates.transpose(0,1)
+            adaptive_reweights = torch.exp(log_uptonow_weights[:, m-1] - logsumexp(log_uptonow_weights[:, m-1], dim=-1).unsqueeze(-1)).detach()
+            adaptive_uptonow_weights = (logsumexp(log_uptonow_weights[:, m-1], dim=-1) - torch.log(torch.FloatTensor([num_particles_rws]))).detach()
+            for l in range(num_particles_rws):
+                ancesters = Categorical(adaptive_reweights).sample()
+                Z = Zs_prevs[torch.arange(batch_size), ancesters]
+                ## z_pairs (B * T-1)-by-(2K)
+                Z_pairs = flatz(Zs_candidates[l], T, K, batch_size)
+                variational, As = enc(Z_pairs, prior_true, batch_size)
+                Zs, log_weights, log_normalizers = smc_hmm_batch(Pi, As, mu_ks, cov_ks, Ys, T, D, K, num_particles_smc, batch_size)
+                Z = smc_resamplings(Zs, log_weights, batch_size)
+                Zs_candidates[l] = Z
+                log_ps = log_joints(prior_true, Z, Pi, As, mu_ks, cov_ks, Ys, T, D, K, batch_size)
+                log_ps_smc = smc_log_joints(Z, Pi, As, mu_ks, cov_ks, Ys, T, D, K, batch_size)
+                log_increment_weights[:, m, l] =  log_ps.detach() + log_normalizers - log_qs(variational, As) - log_ps_smc.detach()
+                log_uptonow_weights[:, m, l] = adaptive_uptonow_weights + log_increment_weights[:, m, l]
+
+    conj_posts = conj_posterior(prior_true.repeat(batch_size, 1, 1), Zs_true, T, K, batch_size)
+    Z_pairs_true = flatz(Zs_true, T, K, batch_size)
+    variational_true, As_notusing = enc(Z_pairs_true, prior_true, batch_size)
+    kls = log_qs(conj_posts, As_true) - log_qs(variational_true, As_true)
+
+    log_overall_weights = log_uptonow_weights[:, -1, :]
+    weights_rws = torch.exp(log_overall_weights - logsumexp(log_overall_weights, dim=1).unsqueeze(1)).detach()
+    uptonow_weights = torch.exp(log_uptonow_weights - logsumexp(log_uptonow_weights, dim=-1).unsqueeze(-1)).detach()
+    ess = (1. / (weights_rws ** 2 ).sum(1)).mean()
+    eubos = torch.mul(weights_rws, log_overall_weights).sum(1)
+    elbos =  log_overall_weights.mean(-1)
+    loss = torch.mul(uptonow_weights, log_increment_weights).sum(1).mean(1)
+    return  loss.mean(), eubos.mean(), elbos.mean(), ess, kls.mean()
+
+
+def ag_sis_stepwise(enc, prior_true, As_true, Zs_true, Pi, mu_ks, cov_ks, Ys, T, D, K, num_particles_rws, num_particles_smc, mcmc_steps, batch_size):
+    log_uptonow_weights = torch.zeros((batch_size, mcmc_steps, num_particles_rws))
+    log_increment_weights = torch.zeros((batch_size, mcmc_steps, num_particles_rws))
+    Zs_candidates = torch.zeros((num_particles_rws, batch_size, T, K))
+    log_normalizers_candidates = torch.zeros((num_particles_rws, batch_size))
+
+    for m in range(mcmc_steps):
+        if m == 0:
+            for l in range(num_particles_rws):
+                ## As B * K * K
+                As = initial_trans(prior_true, K, batch_size)
+                Zs, log_weights, log_normalizers = smc_hmm_batch(Pi, As, mu_ks, cov_ks, Ys, T, D, K, num_particles_smc, batch_size)
+                log_normalizers_candidates[l] = log_normalizers
+                ## Z B * T * K
+                Z = smc_resamplings(Zs, log_weights, batch_size)
+                Zs_candidates[l] = Z
+                ## the first incremental weight is just log normalizer since As is sampled from prior
+                log_increment_weights[:, m, l] = log_normalizers
+                log_uptonow_weights[:, m, l] = log_normalizers
+
+        else:
+            for l in range(num_particles_rws):
+                ## z_pairs (B * T-1)-by-(2K)
+                Z_pairs = flatz(Zs_candidates[l], T, K, batch_size)
+                variational, As = enc(Z_pairs, prior_true, batch_size)
+                Zs, log_weights, log_normalizers = smc_hmm_batch(Pi, As, mu_ks, cov_ks, Ys, T, D, K, num_particles_smc, batch_size)
+                Z = smc_resamplings(Zs, log_weights, batch_size)
+                Zs_candidates[l] = Z
+                log_ps = log_joints(prior_true, Z, Pi, As, mu_ks, cov_ks, Ys, T, D, K, batch_size)
+                log_ps_smc = smc_log_joints(Z, Pi, As, mu_ks, cov_ks, Ys, T, D, K, batch_size)
+                log_increment_weights[:, m, l] =  log_ps.detach() + log_normalizers - log_qs(variational, As) - log_ps_smc.detach()
+                log_uptonow_weights[:, m, l] = log_uptonow_weights[:, m-1, l] + log_increment_weights[:, m, l]
+
+    conj_posts = conj_posterior(prior_true.repeat(batch_size, 1, 1), Zs_true, T, K, batch_size)
+    Z_pairs_true = flatz(Zs_true, T, K, batch_size)
+    variational_true, As_notusing = enc(Z_pairs_true, prior_true, batch_size)
+    kls = log_qs(conj_posts, As_true) - log_qs(variational_true, As_true)
+
+    log_overall_weights = log_uptonow_weights[:, -1, :]
+    weights_rws = torch.exp(log_overall_weights - logsumexp(log_overall_weights, dim=1).unsqueeze(1)).detach()
+    uptonow_weights = torch.exp(log_uptonow_weights - logsumexp(log_uptonow_weights, dim=-1).unsqueeze(-1)).detach()
+    ess = (1. / (weights_rws ** 2 ).sum(1)).mean()
+    eubos = torch.mul(weights_rws, log_overall_weights).sum(1)
+    elbos =  log_overall_weights.mean(-1)
+    loss = torch.mul(uptonow_weights, log_increment_weights).sum(1).mean(1)
+    return  loss.mean(), eubos.mean(), elbos.mean(), ess, kls.mean()
+
+
+
+
+
+
+def ag_sis(enc, prior_true, As_true, Zs_true, Pi, mu_ks, cov_ks, Ys, T, D, K, num_particles_rws, num_particles_smc, mcmc_steps, batch_size):
+    log_overall_weights = torch.zeros((batch_size, num_particles_rws))
+    log_increment_weights = torch.zeros((batch_size, mcmc_steps, num_particles_rws))
+    Zs_candidates = torch.zeros((num_particles_rws, batch_size, T, K))
+
+    for m in range(mcmc_steps):
+        if m == 0:
+            for l in range(num_particles_rws):
+                ## As B * K * K
+                As = initial_trans(prior_true, K, batch_size)
                 Zs, log_weights, log_normalizers = smc_hmm_batch(Pi, As, mu_ks, cov_ks, Ys, T, D, K, num_particles_smc, batch_size)
                 ## Z B * T * K
                 Z = smc_resamplings(Zs, log_weights, batch_size)
                 Zs_candidates[l] = Z
-                ## z_pairs (B * T-1)-by-(2K)
-                Z_pairs = flatz(Z, T, K, batch_size)
                 ## the first incremental weight is just log normalizer since As is sampled from prior
-                log_overall_weights[:, l] =  - log_qs(prior_true.repeat(batch_size, 1, 1), As)
+                log_increment_weights[:, m, l] = log_normalizers
+                log_overall_weights[:, l] = log_normalizers
         else:
             for l in range(num_particles_rws):
-
+                ## z_pairs (B * T-1)-by-(2K)
+                Z_pairs = flatz(Zs_candidates[l], T, K, batch_size)
                 variational, As = enc(Z_pairs, prior_true, batch_size)
                 ## log_q(\phi_t | z_t-1)
-                log_overall_weights[:, l] = log_overall_weights[:, l] - log_qs(variational, As)
+                # log_overall_weights[:, l] = log_overall_weights[:, l] - log_qs(variational, As)
                 Zs, log_weights, log_normalizers = smc_hmm_batch(Pi, As, mu_ks, cov_ks, Ys, T, D, K, num_particles_smc, batch_size)
                 Z = smc_resamplings(Zs, log_weights, batch_size)
                 Zs_candidates[l] = Z
-                Z_pairs = flatz(Z, T, K, batch_size)
                 # log_qs_smc = smc_log_joints(Z, Pi, As, mu_ks, cov_ks, Ys, T, D, K, batch_size)
-                variational, As_notusing = enc(Z_pairs, prior_true, batch_size)
-                log_overall_weights[:, l] = log_overall_weights[:, l] + log_qs(variational, As_candidates[l].clone())
-                As_candidates[l] = As
-                if m == mcmc_steps-1:
-                    log_overall_weights[:, l] = log_overall_weights[:, l] + log_normalizers + log_qs(prior_true.repeat(batch_size, 1, 1), As)
+                log_ps = log_joints(prior_true, Z, Pi, As, mu_ks, cov_ks, Ys, T, D, K, batch_size)
+                log_ps_smc = smc_log_joints(Z, Pi, As, mu_ks, cov_ks, Ys, T, D, K, batch_size)
+                log_increment_weights[:, m, l] = log_overall_weights[:, l].clone() + log_ps.detach() + log_normalizers - log_qs(variational, As) - log_ps_smc.detach()
+                log_overall_weights[:, l] = log_overall_weights[:, l] + log_ps.detach() + log_normalizers - log_qs(variational, As) - log_ps_smc.detach()
+
 
     conj_posts = conj_posterior(prior_true.repeat(batch_size, 1, 1), Zs_true, T, K, batch_size)
     Z_pairs_true = flatz(Zs_true, T, K, batch_size)
@@ -95,43 +198,12 @@ def ag_adapt(enc, prior_true, As_true, Zs_true, Pi, mu_ks, cov_ks, Ys, T, D, K, 
     kls = log_qs(conj_posts, As_true) - log_qs(variational_true, As_true)
 
     weights_rws = torch.exp(log_overall_weights - logsumexp(log_overall_weights, dim=1).unsqueeze(1)).detach()
+    intrement_weights = torch.exp(log_increment_weights - logsumexp(log_increment_weights, dim=-1).unsqueeze(-1)).detach()
     ess = (1. / (weights_rws ** 2 ).sum(1)).mean()
     eubos = torch.mul(weights_rws, log_overall_weights).sum(1)
     elbos =  log_overall_weights.mean(-1)
-    loss = torch.mul(torch.exp(log_overall_weights).detach(), log_overall_weights).sum(1)
+    loss = torch.mul(intrement_weights, log_increment_weights).sum(1).mean(1)
     return  loss.mean(), eubos.mean(), elbos.mean(), ess, kls.mean()
-
-
-def ag_sis(enc, alpha_trans_0, prior_true, Pi, mu_ks, cov_ks, Ys, T, D, K, num_particles_rws, num_particles_smc, mcmc_steps, batch_size):
-    log_overall_weights = torch.zeros((batch_size, num_particles_rws))
-    for l in range(num_particles_rws):
-        ## As B * K * K
-        As = initial_trans(prior_true, K, batch_size)
-        Zs, log_weights, log_normalizers = smc_hmm_batch(Pi, As, mu_ks, cov_ks, Ys, T, D, K, num_particles_smc, batch_size)
-        ## Z B * T * K
-        Z = smc_resamplings(Zs, log_weights, batch_size)
-        ## z_pairs (B * T-1)-by-(2K)
-        Z_pairs = flatz(Z, T, K, batch_size)
-        ## the first incremental weight is just log normalizer since As is sampled from prior
-        log_overall_weights[:, l] =  log_normalizers
-        for m in range(mcmc_steps):
-            As_prev = As.clone()
-            variational, As = enc(Z_pairs, prior_true, batch_size)
-            log_qs_phi = log_qs(variational, As)
-            Zs, log_weights, log_normalizers = smc_hmm_batch(Pi, As, mu_ks, cov_ks, Ys, T, D, K, num_particles_smc, batch_size)
-            Z = smc_resamplings(Zs, log_weights, batch_size)
-            Z_pairs = flatz(Z, T, K, batch_size)
-            log_qs_smc = smc_log_joints(Z, Pi, As, mu_ks, cov_ks, Ys, T, D, K, batch_size)
-            log_ps = log_joints(prior_true, Z, Pi, As, mu_ks, cov_ks, Ys, T, D, K, batch_size).detach()
-
-            log_overall_weights[:, l] = log_ps - log_qs_smc + log_normalizers - log_qs_phi + log_overall_weights[:, l]
-
-    weights_rws = torch.exp(log_overall_weights - logsumexp(log_overall_weights, dim=1).unsqueeze(1)).detach()
-    ess = (1. / (weights_rws ** 2 ).sum(1)).mean()
-    loss =  torch.mul(weights_rws, log_overall_weights).sum(1)
-    eubos = torch.mul(weights_rws, log_overall_weights).sum(1)
-    elbos =  log_overall_weights.mean(-1)
-    return  eubos.mean(), elbos.mean(), variational, ess
 
 def encode_obs(enc, prior_true, Pi, mu_ks, cov_ks, Ys, T, D, K, num_particles_rws, num_particles_smc, batch_size):
     log_overall_weights = torch.zeros((batch_size, num_particles_rws))
