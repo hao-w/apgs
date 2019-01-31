c1ceb9b726caf9bcab7cf23be9ed3f46397f55a3
hao-w <wuhaomxhy@gmail.com>
2019-01-31 15:19:51 -0500

    implemented the new EUBO

--- ag_mcmc/util.py
+++ ag_mcmc/util.py
@@ -5,16 +5,22 @@ from torch.distributions.multivariate_normal import MultivariateNormal
 from torch.distributions.one_hot_categorical import OneHotCategorical as cat
 from torch.distributions.categorical import Categorical
 
-def save_params(ELBOs, KLs, PATH_ENC):
+def save_params(EUBOs, ELBOs, ESSs, PATH_ENC):
     with open(PATH_ENC + 'ELBOs.txt', 'w+') as felbo:
         for elbo in ELBOs:
             felbo.write("%s\n" % elbo)
-    with open(PATH_ENC + 'KLs.txt', 'w+') as fkl:
-        for kl in KLs:
-            fkl.write("%s\n" % kl)
+    with open(PATH_ENC + 'EUBOs.txt', 'w+') as feubo:
+        for eubo in EUBOs:
+            feubo.write("%s\n" % eubo)
+            
+    with open(PATH_ENC + 'ESSs.txt', 'w+') as fess:
+        for ess in ESSs:
+            fess.write("%s\n" % ess)
     felbo.close()
-    fkl.close()
-
+    feubo.close()
+    fess.close()
+    
+    
 def conj_posterior(priors, Zs, T, K, batch_size):
     """
     Zs is B-T-K tensor
@@ -57,14 +63,14 @@ def log_joints_ball(alpha_trans_0, Z, Pi, As, mu_ks, cov_ks, Ys, T, D, K, batch_
     log_probs = log_probs + (Dirichlet(alpha_trans_0).log_prob(As)).sum(1) ## prior of A
     return log_probs
 
-def log_joints(alpha_trans_0, Z, Pi, As, mu_ks, cov_ks, Ys, T, D, K, batch_size):
+def log_joints(prior, Z, Pi, As, mus, covs, Ys, T, D, K, batch_size):
     log_probs = torch.zeros(batch_size).float()
     labels = Z.nonzero()
-    log_probs = log_probs + (MultivariateNormal(mu_ks[labels[:, -1]].view(batch_size, T, D), cov_ks[labels[:, -1]].view(batch_size, T, D, D)).log_prob(Ys)).sum(1) # likelihood of obs
+    log_probs = log_probs + (MultivariateNormal(mus[labels[:, -1]].view(batch_size, T, D), covs[labels[:, -1]].view(batch_size, T, D, D)).log_prob(Ys)).sum(1) # likelihood of obs
     log_probs = log_probs + cat(Pi).log_prob(Z[:, 0]) # z_1 | pi
     inds = labels.view(batch_size, T, 3)[:, :-1, :].contiguous().view(batch_size * (T-1), 3)
     log_probs = log_probs + (cat(As[inds[:, 0], inds[:, -1]].view(batch_size, T-1, K)).log_prob(Z[:, 1:])).sum(1)
-    log_probs = log_probs + (Dirichlet(alpha_trans_0).log_prob(As)).sum(1) ## prior of A
+    log_probs = log_probs + (Dirichlet(prior).log_prob(As)).sum(1) ## prior of A
     return log_probs
 
 def kl_dirichlets(p, q):
