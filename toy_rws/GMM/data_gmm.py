836f2873d8fd7c78a36f2b400a0582debb4aef53
hao-w <wuhaomxhy@gmail.com>
2019-02-11 16:07:49 -0500

    integrate true sigmas into t encoder to see if it improves performance

--- toy_rws/GMM/data_gmm.py
+++ toy_rws/GMM/data_gmm.py
@@ -4,18 +4,19 @@ from torch.distributions.multivariate_normal import MultivariateNormal as mvn
 from torch.distributions.one_hot_categorical import OneHotCategorical as cat
 from torch.distributions.gamma import Gamma
 from torch.distributions.normal import Normal
+from torch.distributions.uniform import Uniform
 import math
 
-def sampling_gmm(T, K, D, ind, radius, prior_covs):
-    # mus_true = torch.zeros((K, D)).float()
-    # for k in range(K):
-    #     if k == 0:
-    #         angle = ind
-    #     else:
-    #         angle = angle + 2 * math.pi / K
-    #         if angle > 2 * math.pi:
-    #             angle -= 2 * math.pi
-    #     mus_true[k] = torch.FloatTensor([math.cos(angle) * radius, math.sin(angle) * radius])
+def sampling_gmm_orbits(T, K, D, ind, radius, prior_covs):
+    mus_true = torch.zeros((K, D)).float()
+    for k in range(K):
+        if k == 0:
+            angle = ind
+        else:
+            angle = angle + 2 * math.pi / K
+            if angle > 2 * math.pi:
+                angle -= 2 * math.pi
+        mus_true[k] = torch.FloatTensor([math.cos(angle) * radius, math.sin(angle) * radius])
     Pi = torch.FloatTensor([1./3, 1./3, 1./3])
     Zs_true = cat(Pi).sample((T,))
     labels = Zs_true.nonzero()[:, 1]
@@ -23,8 +24,17 @@ def sampling_gmm(T, K, D, ind, radius, prior_covs):
         covs_true = torch.eye(D).unsqueeze(0).repeat(K, 1, 1)
         Xs = mvn(mus_true[labels], covs_true[labels]).sample()
     else:
-        precisions = Gamma(torch.ones((K, D))*4.0, torch.ones((K, D)) * 4.0).sample()
-        covs_true  = 1. / precisions
-        mus_true = Normal(torch.zeros((K, D)), covs_true * radius).sample()
-        Xs = Normal(mus_true[labels], torch.sqrt(covs_true[labels])).sample()
+        covs_true = Gamma(torch.ones((K, D))*6.0, torch.ones((K, D)) * 6.0).sample()
+        Xs = Normal(mus_true[labels], covs_true[labels]).sample()
+    return Xs, mus_true, covs_true, Zs_true, Pi
+
+def sampling_gmm(T, K, D, boundary):
+    mus_true = torch.zeros((K, D)).float()
+    for k in range(K):
+        mus_true[k] = Uniform(0, boundary).sample((2,)) * torch.from_numpy(np.random.choice([-1, 1], 2)).float()
+    Pi = torch.FloatTensor([1./3, 1./3, 1./3])
+    Zs_true = cat(Pi).sample((T,))
+    labels = Zs_true.nonzero()[:, 1]
+    covs_true = Gamma(torch.ones((K, D))*6.0, torch.ones((K, D)) * 6.0).sample()
+    Xs = Normal(mus_true[labels], covs_true[labels]).sample()
     return Xs, mus_true, covs_true, Zs_true, Pi
