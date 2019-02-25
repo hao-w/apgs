3ab9c9bfc43a6c13688415b30dfc1ff8d9248e4b
hao-w <wuhaomxhy@gmail.com>
2019-02-25 16:08:26 -0500

    conjugate-prior would put all clusters around the center

--- amorgibbs/GMM/data_gmm.py
+++ amorgibbs/GMM/data_gmm.py
@@ -32,19 +32,24 @@ def sampling_gmm_uniform(T, K, D, boundary):
     mus_true = torch.zeros((K, D)).float()
     for k in range(K):
         mus_true[k] = Uniform(0, boundary).sample((2,)) * torch.from_numpy(np.random.choice([-1, 1], 2)).float()
-    Pi = torch.FloatTensor([1./3, 1./3, 1./3])
+    # Pi = torch.FloatTensor([1./3, 1./3, 1./3])
+    Pi = torch.FloatTensor([1./4, 1./4, 1/4., 1./4])
+
     Zs_true = cat(Pi).sample((T,))
     labels = Zs_true.nonzero()[:, 1]
-    covs_true = Gamma(torch.ones((K, D))*6.0, torch.ones((K, D)) * 6.0).sample()
-    Xs = Normal(mus_true[labels], covs_true[labels]).sample()
-    return Xs, mus_true, covs_true, Zs_true, Pi
+    precisions = Gamma(torch.ones((K, D))*2.0, torch.ones((K, D)) * 2.0).sample()
+    sigmas_true = 1. / torch.sqrt(precisions)
+    Xs = Normal(mus_true[labels], sigmas_true[labels]).sample()
+    return Xs, mus_true, sigmas_true, Zs_true, Pi
 
 def sampling_gmm_conjugate(T, K, D):
-    precisions = Gamma(torch.ones((K, D)) * 2, torch.ones((K,D)) * 2).sample()
+    precisions = Gamma(torch.ones((K, D)) * 3, torch.ones((K,D)) * 3).sample()
     sigmas_true = 1. / torch.sqrt(precisions)
-    mus_sigmas = sigmas_true / 0.5
+    mus_sigmas = sigmas_true / 0.3
     mus_true = Normal(torch.zeros((K, D)), mus_sigmas).sample()
+    # Pi = torch.FloatTensor([1./3, 1./3, 1./ 3])
     Pi = torch.FloatTensor([1./4, 1./4, 1/4., 1./4])
+
     Zs_true = cat(Pi).sample((T,))
     labels = Zs_true.nonzero()[:, 1]
     Xs = Normal(mus_true[labels], sigmas_true[labels]).sample()
