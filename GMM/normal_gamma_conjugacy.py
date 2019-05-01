fixed the the bug in computing conjugate posterior of z, where the loglikelihood should be added with the log of prior, rather than multiplication

GMM/normal_gamma_conjugacy.py
@@ -68,7 +68,7 @@ def Post_z(obs, obs_sigma, obs_mu, prior_pi, N, K):
     obs_mu_expand = obs_mu.unsqueeze(-2).repeat(1, 1, 1, N, 1) # S * B * K * N * D
     obs_sigma_expand = obs_sigma.unsqueeze(-2).repeat(1, 1, 1, N, 1) # S * B * K * N * D
     obs_expand = obs.unsqueeze(2).repeat(1, 1, K, 1, 1) #  S * B * K * N * D
-    log_gammas = Normal(obs_mu_expand, obs_sigma_expand).log_prob(obs_expand).sum(-1).transpose(-1, -2) * prior_pi # S * B * N * K
+    log_gammas = Normal(obs_mu_expand, obs_sigma_expand).log_prob(obs_expand).sum(-1).transpose(-1, -2) + prior_pi.log() # S * B * N * K
     post_logits = F.softmax(log_gammas, dim=-1).log()
     return post_logits
 

GMM/normal_gamma_kls.py
@@ -37,7 +37,7 @@ def kls_NGs(q_alpha, q_beta, q_mu, q_nu, p_alpha, p_beta, p_mu, p_nu):
 
 from torch._six import inf
 
-def kl_cat_cat(p_logits, q_logits, EPS=1e-12):
+def kl_cat_cat(p_logits, q_logits, EPS=1e-15):
     p_probs= p_logits.exp()
     ## To prevent from infinite KL due to ill-defined support of q
     q_logits[q_logits == -inf] = torch.log(torch.FloatTensor([EPS]))