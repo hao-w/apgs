a04f5c0af71bcbb37759a51be77fa5f1efdc2970
Heiko Zimmermann <mail@heikozimmermann.com>
2019-05-06 16:19:08 -0400

    changed the interface of Gibbs_z

--- GMM/oneshot/model_naive_natparam.py
+++ GMM/oneshot/model_naive_natparam.py
@@ -98,24 +98,37 @@ class Enc_z(nn.Module):
         return state
 
 ## need to write this gibbs-z togther with sample prior function a a Class
-def Gibbs_z(obs, obs_sigma, obs_mu, N, K, CUDA, device):
+class Gibbs_z()
     """
     Gibbs sampling for p(z | mu, tau, x) given mu, tau, x
     """
-    q = probtorch.Trace()
-    p = probtorch.Trace()
-    prior_pi = torch.ones(K) * (1./ K)
-    if CUDA:
-        prior_pi = prior_pi.cuda().to(device)
-    obs_mu_expand = obs_mu.unsqueeze(-2).repeat(1, 1, 1, N, 1) # S * B * K * N * D
-    obs_sigma_expand = obs_sigma.unsqueeze(-2).repeat(1, 1, 1, N, 1) # S * B * K * N * D
-    obs_expand = obs.unsqueeze(2).repeat(1, 1, K, 1, 1) #  S * B * K * N * D
-    log_gammas = Normal(obs_mu_expand, obs_sigma_expand).log_prob(obs_expand).sum(-1).transpose(-1, -2) + prior_pi.log() # S * B * N * K
-    q_probs = F.softmax(log_gammas, dim=-1)
+    def __init__(self, prior_pi==None, CUDA, device):
+        if prior_pi is None:
+            prior_pi = torch.ones(K) * (1./ K)
+        self.prior_pi = prior_pi
 
-    _ = q.variable(cat, probs=q_probs, value=z, name='zs')
-    _ = p.variable(cat, probs=prior_pi, value=z, name='zs')
-    return post_logits
+        if CUDA:
+            self.prior_pi = self.prior_pi.cuda().to(device)
+
+
+    def forward(self, obs, obs_sigma, obs_mu, N, K):
+        q = probtorch.Trace()
+        p = probtorch.Trace()
+        
+        obs_mu_expand = obs_mu.unsqueeze(-2).repeat(1, 1, 1, N, 1) # S * B * K * N * D
+        obs_sigma_expand = obs_sigma.unsqueeze(-2).repeat(1, 1, 1, N, 1) # S * B * K * N * D
+        obs_expand = obs.unsqueeze(2).repeat(1, 1, K, 1, 1) #  S * B * K * N * D
+        log_gammas = Normal(obs_mu_expand, obs_sigma_expand).log_prob(obs_expand).sum(-1).transpose(-1, -2) + self.prior_pi.log() # S * B * N * K
+        q_probs = F.softmax(log_gammas, dim=-1)
+
+        _ = q.variable(cat, probs=q_probs, value=z, name='zs')
+        _ = p.variable(cat, probs=self.prior_pi, value=z, name='zs')
+        return q, p
+
+    def sample_prior(self, N, sample_size, batch_size):
+        p_init_z = cat(self.prior_pi)
+        state = p_init_z.sample((sample_size, batch_size, N,))
+        return state
 
 def initialize(K, D, num_hidden_local, CUDA, device, LR):
     enc_eta = Enc_eta(K, D, CUDA, device)
