d9286826cf3107052156667f1903bf5a301c29e6
hao-w <wuhaomxhy@gmail.com>
2019-05-06 22:14:42 -0400

    implemented close-form-z

--- GMM/amorgibbs/eubo.py
+++ GMM/amorgibbs/eubo.py
@@ -5,6 +5,120 @@ from normal_gamma_kls import *
 from normal_gamma_conjugacy import *
 import probtorch
 
+def Eubo_oneshot_eta_cfz(oneshot_eta, enc_eta, gibbs_z, obs, N, K, D, mcmc_size, sample_size, batch_size, device, idw_flag=True):
+    """
+    initialize eta
+    adaptive resampling after each step
+    individually compute importance weights
+    """
+    eubos = torch.zeros(mcmc_size+1).cuda().to(device)
+    elbos = torch.zeros(mcmc_size+1).cuda().to(device)
+    esss = torch.zeros(mcmc_size+1).cuda().to(device)
+    ## initialize mu, tau from the prior
+    q_eta, p_eta, q_nu = oneshot_eta(obs, K, D)
+    log_p_eta = p_eta['means'].log_prob.sum(-1) + p_eta['precisions'].log_prob.sum(-1)
+    log_q_eta = q_eta['means'].log_prob.sum(-1) + q_eta['precisions'].log_prob.sum(-1)
+    obs_mu = q_eta['means'].value
+    obs_tau = q_eta['precisions'].value
+    q_z, p_z = gibbs_z.forward(obs, obs_tau, obs_mu, N, K)
+    log_p_z = p_z['zs'].log_prob
+    log_q_z = q_z['zs'].log_prob
+    state = q_z['zs'].value ## S * B * N * K
+    log_obs_n = Log_likelihood(obs, state, obs_mu, 1. / obs_tau.sqrt(), K, D, cluster_flag=False)
+    log_weights = log_obs_n.sum(-1) + log_p_eta.sum(-1) - log_q_eta.sum(-1) + log_p_z.sum(-1) - log_q_z.sum(-1)
+    weights = F.softmax(log_weights, 0).detach()
+    ## EUBO, ELBO, ESS
+    eubos[0] = (weights * log_weights).sum(0).mean()
+    elbos[0] = log_weights.mean()
+    esss[0] = (1. / (weights**2).sum(0)).mean()
+    for m in range(mcmc_size):
+        if m == 0:
+            ## resample z -- cluster assignments
+            state = resample_state(state, weights, idw_flag=False)
+        else:
+            state = resample_state(state, weights_local, idw_flag=idw_flag)
+        ## update tau and mu -- global variables
+        q_eta, p_eta, q_nu = enc_eta(obs, state, K, D)
+        log_p_eta = p_eta['means'].log_prob.sum(-1) + p_eta['precisions'].log_prob.sum(-1)
+        log_q_eta = q_eta['means'].log_prob.sum(-1) + q_eta['precisions'].log_prob.sum(-1)
+        obs_mu = q_eta['means'].value
+        obs_tau = q_eta['precisions'].value
+        log_obs_k = Log_likelihood(obs, state, obs_mu, 1. / obs_tau.sqrt(), K, D, cluster_flag=True)
+        log_weights_global = log_obs_k + log_p_eta - log_q_eta
+        weights_global = F.softmax(log_weights_global, 0).detach()
+        ## resample eta -- mu and tau
+        obs_mu, obs_tau = resample_eta(obs_mu, obs_tau, weights_global, idw_flag=idw_flag)
+        ## update z -- cluster assignments
+        q_z, p_z = gibbs_z.forward(obs, obs_tau, obs_mu, N, K)
+        log_p_z = p_z['zs'].log_prob
+        log_q_z = q_z['zs'].log_prob
+        state = q_z['zs'].value ## S * B * N * K
+        log_obs_n = Log_likelihood(obs, state, obs_mu, 1. / obs_tau.sqrt(), K, D, cluster_flag=False)
+        log_weights_local = log_obs_n + log_p_z - log_q_z
+        weights_local = F.softmax(log_weights_local, 0).detach()
+        ## EUBO, ELBO, ESS
+        eubos[m+1] =((weights_global * log_weights_global).sum(0).sum(-1).mean() + (weights_local * log_weights_local).sum(0).sum(-1).mean()) / 2
+        elbos[m+1] = (log_weights_global.sum(-1).mean() + log_weights_local.sum(-1).mean()) / 2
+        esss[m+1] = ((1. / (weights_local**2).sum(0)).mean() + (1. / (weights_global**2).sum(0)).mean() ) / 2
+    return eubos, elbos, esss, q_eta, p_eta, q_z, p_z, q_nu, enc_eta.prior_nu
+
+def Eubo_oneshot_eta(oneshot_eta, enc_eta, enc_z, obs, N, K, D, mcmc_size, sample_size, batch_size, device, idw_flag=True):
+    """
+    initialize eta
+    adaptive resampling after each step
+    individually compute importance weights
+    """
+    eubos = torch.zeros(mcmc_size+1).cuda().to(device)
+    elbos = torch.zeros(mcmc_size+1).cuda().to(device)
+    esss = torch.zeros(mcmc_size+1).cuda().to(device)
+    ## initialize mu, tau from the prior
+    q_eta, p_eta, q_nu = oneshot_eta(obs, K, D)
+    log_p_eta = p_eta['means'].log_prob.sum(-1) + p_eta['precisions'].log_prob.sum(-1)
+    log_q_eta = q_eta['means'].log_prob.sum(-1) + q_eta['precisions'].log_prob.sum(-1)
+    obs_mu = q_eta['means'].value
+    obs_tau = q_eta['precisions'].value
+    q_z, p_z = enc_z(obs, obs_tau, obs_mu, N, sample_size, batch_size)
+    log_p_z = p_z['zs'].log_prob
+    log_q_z = q_z['zs'].log_prob
+    state = q_z['zs'].value ## S * B * N * K
+    log_obs_n = Log_likelihood(obs, state, obs_mu, 1. / obs_tau.sqrt(), K, D, cluster_flag=False)
+    log_weights = log_obs_n.sum(-1) + log_p_eta.sum(-1) - log_q_eta.sum(-1) + log_p_z.sum(-1) - log_q_z.sum(-1)
+    weights = F.softmax(log_weights, 0).detach()
+    ## EUBO, ELBO, ESS
+    eubos[0] = (weights * log_weights).sum(0).mean()
+    elbos[0] = log_weights.mean()
+    esss[0] = (1. / (weights**2).sum(0)).mean()
+    for m in range(mcmc_size):
+        if m == 0:
+            ## resample z -- cluster assignments
+            state = resample_state(state, weights, idw_flag=False)
+        else:
+            state = resample_state(state, weights_local, idw_flag=idw_flag)
+        ## update tau and mu -- global variables
+        q_eta, p_eta, q_nu = enc_eta(obs, state, K, D)
+        log_p_eta = p_eta['means'].log_prob.sum(-1) + p_eta['precisions'].log_prob.sum(-1)
+        log_q_eta = q_eta['means'].log_prob.sum(-1) + q_eta['precisions'].log_prob.sum(-1)
+        obs_mu = q_eta['means'].value
+        obs_tau = q_eta['precisions'].value
+        log_obs_k = Log_likelihood(obs, state, obs_mu, 1. / obs_tau.sqrt(), K, D, cluster_flag=True)
+        log_weights_global = log_obs_k + log_p_eta - log_q_eta
+        weights_global = F.softmax(log_weights_global, 0).detach()
+        ## resample eta -- mu and tau
+        obs_mu, obs_tau = resample_eta(obs_mu, obs_tau, weights_global, idw_flag=idw_flag)
+        ## update z -- cluster assignments
+        q_z, p_z = enc_z(obs, obs_tau, obs_mu, N, sample_size, batch_size)
+        log_p_z = p_z['zs'].log_prob
+        log_q_z = q_z['zs'].log_prob
+        state = q_z['zs'].value ## S * B * N * K
+        log_obs_n = Log_likelihood(obs, state, obs_mu, 1. / obs_tau.sqrt(), K, D, cluster_flag=False)
+        log_weights_local = log_obs_n + log_p_z - log_q_z
+        weights_local = F.softmax(log_weights_local, 0).detach()
+        ## EUBO, ELBO, ESS
+        eubos[m+1] =((weights_global * log_weights_global).sum(0).sum(-1).mean() + (weights_local * log_weights_local).sum(0).sum(-1).mean()) / 2
+        elbos[m+1] = (log_weights_global.sum(-1).mean() + log_weights_local.sum(-1).mean()) / 2
+        esss[m+1] = ((1. / (weights_local**2).sum(0)).mean() + (1. / (weights_global**2).sum(0)).mean() ) / 2
+    return eubos, elbos, esss, q_eta, p_eta, q_z, p_z, q_nu, enc_eta.prior_nu
+
 def Eubo_idw_init_z(enc_eta, enc_z, obs, N, K, D, mcmc_size, sample_size, batch_size, device, idw_flag=True):
     """
     initialize z
--- GMM/amorgibbs/model_naive_natparam.py
+++ GMM/amorgibbs/model_naive_natparam.py
@@ -6,6 +6,55 @@ from torch.distributions.one_hot_categorical import OneHotCategorical as cat
 from torch.distributions.gamma import Gamma
 from normal_gamma_conjugacy import *
 
+class Oneshot_eta(nn.Module):
+    def __init__(self, K, D, CUDA, device):
+        super(self.__class__, self).__init__()
+
+        self.gamma = nn.Sequential(
+            nn.Linear(D, K),
+            nn.Softmax(-1))
+
+        self.ob = nn.Sequential(
+            nn.Linear(D, D))
+
+        self.prior_mu = torch.zeros((K, D))
+        self.prior_nu = torch.ones((K, D)) * 0.3
+        self.prior_alpha = torch.ones((K, D)) * 4
+        self.prior_beta = torch.ones((K, D)) * 4
+        if CUDA:
+            self.prior_mu = self.prior_mu.cuda().to(device)
+            self.prior_nu = self.prior_nu.cuda().to(device)
+            self.prior_alpha = self.prior_alpha.cuda().to(device)
+            self.prior_beta = self.prior_beta.cuda().to(device)
+
+    def forward(self, obs, K, D):
+        q = probtorch.Trace()
+        p = probtorch.Trace()
+        gammas = self.gamma(obs) # S * B * N * K --> S * B * N * K
+        xs = self.ob(obs)  # S * B * N * D --> S * B * N * D
+        q_alpha, q_beta, q_mu, q_nu = Post_eta(xs, gammas,
+                                                 self.prior_alpha, self.prior_beta, self.prior_mu, self.prior_nu, K, D)
+        precisions = Gamma(q_alpha, q_beta).sample()
+        q.gamma(q_alpha,
+                q_beta,
+                value=precisions,
+                name='precisions')
+        p.gamma(self.prior_alpha,
+                self.prior_beta,
+                value=q['precisions'],
+                name='precisions')
+        means = Normal(q_mu, 1. / (q_nu * q['precisions'].value).sqrt()).sample()
+        q.normal(q_mu,
+                 1. / (q_nu * q['precisions'].value).sqrt(),
+                 value=means,
+                 name='means')
+        p.normal(self.prior_mu,
+                 1. / (self.prior_nu * p['precisions'].value).sqrt(),
+                 value=q['means'],
+                 name='means')
+        return q, p, q_nu
+
+
 class Enc_eta(nn.Module):
     def __init__(self, K, D, CUDA, device):
         super(self.__class__, self).__init__()
@@ -54,14 +103,13 @@ class Enc_eta(nn.Module):
                  value=q['means'],
                  name='means')
         return q, p, q_nu
-        
+
     def sample_prior(self, sample_size, batch_size):
         p_tau = Gamma(self.prior_alpha, self.prior_beta)
         obs_tau = p_tau.sample((sample_size, batch_size,))
         p_mu = Normal(self.prior_mu.repeat(sample_size, batch_size, 1, 1), 1. / (self.prior_nu * obs_tau).sqrt())
         obs_mu = p_mu.sample()
-        obs_sigma = 1. / obs_tau.sqrt()
-        return obs_mu, obs_sigma
+        return obs_mu, obs_tau
 
 class Enc_z(nn.Module):
     def __init__(self, K, D, num_hidden, CUDA, device):
@@ -98,11 +146,52 @@ class Enc_z(nn.Module):
         state = p_init_z.sample((sample_size, batch_size, N,))
         return state
 
-def initialize(K, D, num_hidden_local, CUDA, device, LR):
-    enc_eta = Enc_eta(K, D, CUDA, device)
-    enc_z = Enc_z(K, D, num_hidden_local, CUDA, device)
-    if CUDA:
-        enc_eta.cuda().to(device)
-        enc_z.cuda().to(device)
-    optimizer =  torch.optim.Adam(list(enc_z.parameters())+list(enc_eta.parameters()),lr=LR, betas=(0.9, 0.99))
-    return enc_eta, enc_z, optimizer
+class Gibbs_z():
+    """
+    Gibbs sampling for p(z | mu, tau, x) given mu, tau, x
+    """
+    def __init__(self, K, CUDA, device):
+
+        self.prior_pi = torch.ones(K) * (1./ K)
+        if CUDA:
+            self.prior_pi = self.prior_pi.cuda().to(device)
+
+    def forward(self, obs, obs_sigma, obs_mu, N, K):
+        q = probtorch.Trace()
+        p = probtorch.Trace()
+
+        obs_mu_expand = obs_mu.unsqueeze(-2).repeat(1, 1, 1, N, 1) # S * B * K * N * D
+        obs_sigma_expand = obs_sigma.unsqueeze(-2).repeat(1, 1, 1, N, 1) # S * B * K * N * D
+        obs_expand = obs.unsqueeze(2).repeat(1, 1, K, 1, 1) #  S * B * K * N * D
+        log_gammas = Normal(obs_mu_expand, obs_sigma_expand).log_prob(obs_expand).sum(-1).transpose(-1, -2) + self.prior_pi.log() # S * B * N * K
+        q_probs = F.softmax(log_gammas, dim=-1)
+        z = cat(q_probs).sample()
+        _ = q.variable(cat, probs=q_probs, value=z, name='zs')
+        _ = p.variable(cat, probs=self.prior_pi, value=z, name='zs')
+        return q, p
+
+    def sample_prior(self, N, sample_size, batch_size):
+        p_init_z = cat(self.prior_pi)
+        state = p_init_z.sample((sample_size, batch_size, N,))
+        return state
+
+def initialize(K, D, num_hidden_local, CUDA, device, LR, closed_form_z=False):
+    if closed_form_z:
+        oneshot_eta = Oneshot_eta(K, D, CUDA, device)
+        enc_eta = Enc_eta(K, D, CUDA, device)
+        enc_z = Gibbs_z(K, CUDA, device)
+        if CUDA:
+            oneshot_eta.cuda().to(device)
+            enc_eta.cuda().to(device)
+        optimizer =  torch.optim.Adam(list(oneshot_eta.parameters())+list(enc_eta.parameters()),lr=LR, betas=(0.9, 0.99))
+    else:
+        oneshot_eta = Oneshot_eta(K, D, CUDA, device)
+        enc_eta = Oneshot_eta(K, D, CUDA, device)
+        enc_z = Enc_z(K, D, num_hidden_local, CUDA, device)
+        if CUDA:
+            oneshot_eta.cuda().to(device)
+            enc_eta.cuda().to(device)
+            enc_z.cuda().to(device)
+        optimizer =  torch.optim.Adam(list(oneshot_eta.parameters())+list(enc_z.parameters())+list(enc_eta.parameters()),lr=LR, betas=(0.9, 0.99))
+    return oneshot_eta, enc_eta, enc_z, optimizer
+
