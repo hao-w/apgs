041159547a6a2376c8b412a0844b8e4fa4ae098c
hao-w <wuhaomxhy@gmail.com>
2020-11-04 20:43:17 -0500

    removed unnecessary files

--- apgs/utils.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-def shuffler(data):
-    DIM1, DIM2, DIM3 = data.shape
-    indices = torch.cat([torch.randperm(DIM2).unsqueeze(0) for b in range(DIM1)])
-    indices_expand = indices.unsqueeze(-1).repeat(1, 1, DIM3)
-    return torch.gather(data, 1, indices_expand)
