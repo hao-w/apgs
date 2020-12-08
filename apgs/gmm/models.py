a29b8a9d476e0010b064db3779ab10dbe560419a
hao-w <wuhaomxhy@gmail.com>
2020-12-08 16:36:51 -0500

    .

--- apgs/gmm/models.py
+++ apgs/gmm/models.py
@@ -14,17 +14,17 @@ class Enc_rws_eta(nn.Module):
     def __init__(self, K, D):
         super(self.__class__, self).__init__()
 
-        self.gamma = nn.Sequential(
+        self.rws_eta_gamma = nn.Sequential(
             nn.Linear(D, K),
             nn.Softmax(-1))
 
-        self.ob = nn.Sequential(
+        self.rws_eta_ob = nn.Sequential(
             nn.Linear(D, D))
 
     def forward(self, ob, prior_ng, sampled=True, tau_old=None, mu_old=None):
         q = probtorch.Trace()
         (prior_alpha, prior_beta, prior_mu, prior_nu) = prior_ng
-        q_alpha, q_beta, q_mu, q_nu = posterior_eta(self.ob(ob) , self.gamma(ob), prior_alpha, prior_beta, prior_mu, prior_nu)
+        q_alpha, q_beta, q_mu, q_nu = posterior_eta(self.rws_eta_ob(ob) , self.rws_eta_gamma(ob), prior_alpha, prior_beta, prior_mu, prior_nu)
         if sampled: ## used in forward transition kernel where we need to sample
             tau = Gamma(q_alpha, q_beta).sample()
             q.gamma(q_alpha,
@@ -53,17 +53,17 @@ class Enc_apg_eta(nn.Module):
     """
     def __init__(self, K, D):
         super(self.__class__, self).__init__()
-        self.gamma = nn.Sequential(
+        self.apg_eta_gamma = nn.Sequential(
             nn.Linear(K+D, K),
             nn.Softmax(-1))
-        self.ob = nn.Sequential(
+        self.apg_eta_ob = nn.Sequential(
             nn.Linear(K+D, D))
 
     def forward(self, ob, z, prior_ng, sampled=True, tau_old=None, mu_old=None):
         q = probtorch.Trace()
         (prior_alpha, prior_beta, prior_mu, prior_nu) = prior_ng
         ob_z = torch.cat((ob, z), -1) # concatenate observations and cluster asssignemnts
-        q_alpha, q_beta, q_mu, q_nu = posterior_eta(self.ob(ob_z) , self.gamma(ob_z), prior_alpha, prior_beta, prior_mu, prior_nu)
+        q_alpha, q_beta, q_mu, q_nu = posterior_eta(self.apg_eta_ob(ob_z) , self.apg_eta_gamma(ob_z), prior_alpha, prior_beta, prior_mu, prior_nu)
         
         if sampled == True:
             tau = Gamma(q_alpha, q_beta).sample()
